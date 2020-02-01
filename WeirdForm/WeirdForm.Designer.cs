6076de106cdae1d1ad118be0e2dc633966a0c149
mskKote <52584520+mskKote@users.noreply.github.com>
2020-02-01 18:13:20 +0300

    Add files via upload

---
+++ WeirdForm/WeirdForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WeirdForm
+{
+    partial class WeirdForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WeirdForm));
+            this.EXITBtn = new System.Windows.Forms.Button();
+            this.Loh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EXITBtn
+            // 
+            this.EXITBtn.BackColor = System.Drawing.Color.AliceBlue;
+            this.EXITBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("EXITBtn.BackgroundImage")));
+            this.EXITBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EXITBtn.Font = new System.Drawing.Font("MS Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EXITBtn.Location = new System.Drawing.Point(0, 0);
+            this.EXITBtn.Name = "EXITBtn";
+            this.EXITBtn.Size = new System.Drawing.Size(319, 162);
+            this.EXITBtn.TabIndex = 0;
+            this.EXITBtn.UseVisualStyleBackColor = false;
+            this.EXITBtn.Click += new System.EventHandler(this.Button1_Click);
+            this.EXITBtn.MouseEnter += new System.EventHandler(this.EXITBtn_MouseEnter);
+            this.EXITBtn.MouseHover += new System.EventHandler(this.EXITBtn_MouseEnter);
+            // 
+            // Loh
+            // 
+            this.Loh.BackColor = System.Drawing.Color.AliceBlue;
+            this.Loh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Loh.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Loh.Location = new System.Drawing.Point(0, 158);
+            this.Loh.Name = "Loh";
+            this.Loh.Size = new System.Drawing.Size(319, 43);
+            this.Loh.TabIndex = 1;
+            this.Loh.Text = "Количество промахов: ";
+            this.Loh.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Loh.UseVisualStyleBackColor = false;
+            this.Loh.MouseEnter += new System.EventHandler(this.EXITBtn_MouseEnter);
+            // 
+            // WeirdForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 200);
+            this.ControlBox = false;
+            this.Controls.Add(this.Loh);
+            this.Controls.Add(this.EXITBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WeirdForm";
+            this.ShowIcon = false;
+            this.MouseHover += new System.EventHandler(this.WeirdForm_MouseHover);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EXITBtn;
+        private System.Windows.Forms.Button Loh;
+    }
+}
+
---
+++ WeirdForm/WeirdForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeirdForm
+{
+    public partial class WeirdForm : Form
+    {
+        static Random Random = new Random();
+        public WeirdForm()
+        {
+            InitializeComponent();
+            this.MouseClick += (object sender, MouseEventArgs e) => BackColor = Color.PaleVioletRed;
+            //this.MouseEnter += (object sender, EventArgs e) => BackColor = DefaultBackColor;
+            //this.MouseDoubleClick += (object sender, MouseEventArgs e) => {
+            //    Size = new Size(new Point(Random.Next(100, 1001), Random.Next(100, 1001)));
+            //};
+            // Virus
+            this.MouseEnter += (object sender, EventArgs e) =>
+            {
+                Location = new Point(Random.Next(0, 1920 - Size.Width),
+                                     Random.Next(0, 880 - Size.Height));
+            };
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+
+        static int LohCounter = 0;
+        string defaultText = "Количество промахов: ";
+
+        private void EXITBtn_MouseEnter(object sender, EventArgs e)
+        {
+            GetAway();
+        }
+
+        private void Button1_MouseEnter(object sender, EventArgs e)
+        {
+            GetAway();
+        }
+
+        private void WeirdForm_MouseHover(object sender, EventArgs e)
+        {
+            GetAway();
+        }
+
+        private void GetAway()
+        {
+            Location = new Point(Random.Next(0, 1920 - Size.Width),
+                                 Random.Next(0, 880 - Size.Height));
+            LohCounter = unchecked(LohCounter + 1);
+            Loh.Text = defaultText + LohCounter;
+        }
+    }
+}
